0575c98ab481b24f8965bdd3dc23234da865e0fa
Bluewind <bluewind@kakao.com>
2023-11-01 16:33:23 +0900

    create project

---
+++ src/main/java/com/bluewind/mukcha/MukchaApplication.java
@@ -0,0 +1,13 @@
+package com.bluewind.mukcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MukchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MukchaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bluewind/mukcha/MukchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluewind.mukcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MukchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
